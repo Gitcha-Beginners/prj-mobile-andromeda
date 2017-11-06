71b80c738dcf39e0d97cc22c13c77336cc6c7c39
rosekim <ryrt12@naver.com>
2017-11-06 18:03:48 +0900

    working

--- member/roseKim/ScoreList/app/src/main/java/com/example/info/scorelist/MainActivity.java
+++ member/roseKim/ScoreList/app/src/main/java/com/example/info/scorelist/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.info.scorelist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    //접수입력 팝업 띄우기
+    public void popupView(View view){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("게임 점수");
+        alert.setMessage("점수를 입력하세요 뀨");
+
+
+        final EditText name = new EditText(this);
+        alert.setView(name);
+
+        alert.setPositiveButton("입력", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String username = name.getText().toString();
+
+
+            }
+        });
+
+
+        alert.setNegativeButton("취소",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+            }
+        });
+
+        alert.show();
     }
 }
